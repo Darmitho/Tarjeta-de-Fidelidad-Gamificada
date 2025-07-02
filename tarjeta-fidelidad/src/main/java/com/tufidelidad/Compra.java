0bc7c69f0aa997215758d735d00a1cf4a9e2232e
Daniel Villarroel <davillarroelm2000@gmail.com>
2025-07-02 13:08:26 -0400

    feat: clase Compra creada con atributos básicos y getters

--- tarjeta-fidelidad/src/main/java/com/tufidelidad/Compra.java
+++ tarjeta-fidelidad/src/main/java/com/tufidelidad/Compra.java
@@ -1,5 +1,33 @@
 package com.tufidelidad;
 
+import java.time.LocalDateTime;
+
 public class Compra {
-    
+    private String idCompra;
+    private String idCliente;
+    private double monto;
+    private LocalDateTime fecha;
+
+    public Compra(String idCompra, String idCliente, double monto, LocalDateTime fecha) {
+        this.idCompra = idCompra;
+        this.idCliente = idCliente;
+        this.monto = monto;
+        this.fecha = fecha;
+    }
+
+    public String getIdCompra() {
+        return idCompra;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
 }
